01759ae5d25a8d0e168b715a71fb4975a74827f8
Dmitry Petrov <dmitrynpetrov@gmail.com>
2020-08-10 21:27:54 +0400

    Register commands explicitly

--- src/main/kotlin/org/alessio29/pbtaBot/PbtaBot.kt
+++ src/main/kotlin/org/alessio29/pbtaBot/PbtaBot.kt
@@ -1,11 +1,15 @@
 package org.alessio29.pbtaBot
 
 import net.dv8tion.jda.api.JDABuilder
+import org.alessio29.pbtaBot.commands.impl.PingCommand
 import org.alessio29.pbtaBot.internal.ParsingInputListener
+import org.alessio29.pbtaBot.internal.commands.CommandRegistry
 import javax.security.auth.login.LoginException
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
+    CommandRegistry.registerCommand(PingCommand)
+
     connect(args[0])
     println("Connected!")
 }
